75d30340c6901c6f101f2bd81742687de70aad42
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-02-12 01:05:50 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery")
+const imgCardMarkup = createimgCardMarkup(galleryItems)
+function createimgCardMarkup(galleryItems) 
+{return galleryItems.map(({preview, original, description})=> 
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    width = "340"/></a></div>`)
+    .join("")
+}
+galleryEl.insertAdjacentHTML('afterbegin', imgCardMarkup);
+
+galleryEl.addEventListener('click', imgOnclick);
+function imgOnclick(ev){
+ev.preventDefault();
+if(!ev.target.dataset.source){
+return;
+}
+var instance = basicLightbox.create(
+`
+<img src="${ev.target.dataset.source}" width ="800" height="auto">
+`, 
+ {
+    onShow: (instance) => {
+      window.addEventListener("keydown", onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", onEscKeyPress);
+    },
+  }
+);
+instance.show();
+
+function onEscKeyPress(e) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = e.code === ESC_KEY_CODE;
+  if (isEscKey) {
+    instance.close();
+  }
+}
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery")
+const imgCardMarkup = createimgCardMarkup(galleryItems)
+function createimgCardMarkup(galleryItems) 
+{return galleryItems.map(({preview, original, description})=> 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}"" alt="${description}"/>
+</a>`)
+    .join("")
+}
+galleryEl.insertAdjacentHTML('afterbegin', imgCardMarkup);
+
+galleryEl.addEventListener('click', imgOnclick);
+function imgOnclick(ev){
+ev.preventDefault();
+const lightbox = new SimpleLightbox('.gallery a', {  captionDelay: 250,
+    captionsData: "alt",})
+
+}
